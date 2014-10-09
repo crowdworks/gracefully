cc8df3ef09eae6b361fd615a9b2fc54210de7846
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2014-10-09 21:03:14 +0900

    Extract Gracefully::Error to error.rb

--- lib/gracefully/try.rb
+++ lib/gracefully/try.rb
@@ -1,3 +1,5 @@
+require_relative 'error'
+
 module Gracefully
   class Try
     def self.to(&block)
@@ -63,37 +65,5 @@ module Gracefully
     def get
       raise Error.new('Tried to get the value of a failure', nested: @error)
     end
-  end
-
-  # Thanks to [nested](https://github.com/skorks/nesty) for the original code
-  module NestedError
-    def initialize(message, args)
-      @nested = args[:nested]
-      super(message)
-    end
-
-    def set_backtrace(backtrace)
-      @raw_backtrace = backtrace
-      if nested
-        backtrace = backtrace - nested_raw_backtrace
-        backtrace += ["#{nested.backtrace.first}: #{nested.message} (#{nested.class.name})"]
-        backtrace += nested.backtrace[1..-1] || []
-      end
-      super(backtrace)
-    end
-
-    private
-
-    def nested_raw_backtrace
-      nested.respond_to?(:raw_backtrace) ? nested.raw_backtrace : nested.backtrace
-    end
-
-    def nested
-      @nested
-    end
-  end
-
-  class Error < StandardError
-    include NestedError
   end
 end
