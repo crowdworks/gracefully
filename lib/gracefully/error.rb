4453a181eef5af4baa50766556624bf21cc4f68c
Yusuke KUOKA <yusuke.kuoka@crowdworks.co.jp>
2014-10-10 15:33:49 +0900

    Make NestedError less complex

--- lib/gracefully/error.rb
+++ lib/gracefully/error.rb
@@ -9,14 +9,18 @@ module Gracefully
     def set_backtrace(backtrace)
       @raw_backtrace = backtrace
       if nested
-        backtrace = backtrace - nested_raw_backtrace
-        backtrace += ["#{nested.backtrace.first}: #{nested.message} (#{nested.class.name})"]
-        backtrace += nested.backtrace[1..-1] || []
+        backtrace = include_nested_raw_backtrace_in backtrace
       end
       super(backtrace)
     end
 
     private
+
+    def include_nested_raw_backtrace_in(backtrace)
+      backtrace = backtrace - nested_raw_backtrace
+      backtrace += ["#{nested.backtrace.first}: #{nested.message} (#{nested.class.name})"]
+      backtrace + nested.backtrace[1..-1] || []
+    end
 
     def nested_raw_backtrace
       nested.respond_to?(:raw_backtrace) ? nested.raw_backtrace : nested.backtrace
